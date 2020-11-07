dbfe5583ddd1c01184a9996ed9fb52ddab75166d
Josh Lemberg <48109659+joshlemberg@users.noreply.github.com>
2020-11-06 21:38:50 -0500

    it doesn't work

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "if you're reading this it's too late"
+application = app
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	return render_template('index.html')
---
+++ static/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	var i = 0;
+	var txt = "his is an example of a typing effect";
+	var speed = 50;
+
+	$("#typingeffect").click(function(event) {
+		while (i < txt.length) {
+			var newHTML = $("#testheader").html() + txt.charAt(i);
+			console.log($("#testheader").html());
+			console.log(txt.charAt(i));
+			console.log("i = " + i);
+    		$("#testheader").html(newHTML);
+    		++i;
+    		// setTimeout(typeWriter, speed);
+  		}
+	});
+
+
+	$("#addbutton").click(function(event) {
+		$("#testheader").html("This is a test");; // Uses jQuery selector :hidden, referenced from https://api.jquery.com/hidden-selector/, and .first() referenced from https://api.jquery.com/first-selector/
+		return false;
+	});
+});
