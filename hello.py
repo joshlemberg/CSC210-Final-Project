19fa56259d13a02c97ba9be559301e7fd3879e43
Josh Lemberg <48109659+joshlemberg@users.noreply.github.com>
2020-11-15 13:16:52 -0500

    idk

--- hello.py
+++ hello.py
@@ -13,6 +13,13 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 def make_shell_context():
 	return dict(db=db, User=User, Role=Role)
 
+@app.cli.command() 
+def test():
+	"""Run the unit tests."""
+	import unittest
+	tests = unittest.TestLoader().discover('tests') 
+	unittest.TextTestRunner(verbosity=2).run(tests)
+
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
 # app = Flask(__name__)
