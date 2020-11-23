75bceb2069cf9fb89488909a1421fed2b309a824
Josh Lemberg <48109659+joshlemberg@users.noreply.github.com>
2020-11-23 15:06:53 -0500

    resend confirmation update
    Resending confirmation email now redirects back to profile page.

--- app/auth/views.py
+++ app/auth/views.py
@@ -58,7 +58,7 @@ def resend_confirmation():
     token = current_user.generate_confirmation_token()
     send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you.')
-    return redirect(url_for('main.index'))
+    return render_template('auth/profile.html')
 
 @auth.route('/profile')
 @login_required
