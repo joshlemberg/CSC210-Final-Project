6a3ae786ea026f030a9a3db54e11f77ee57d84f1
Josh Lemberg <48109659+joshlemberg@users.noreply.github.com>
2020-12-08 15:51:44 -0500

    h

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Shape Shack]'
-    FLASKY_MAIL_SENDER = 'Shape Shake <theshapeshackbusiness@gmail.com>'
+    FLASKY_MAIL_SENDER = 'Shape Shack <theshapeshackbusiness@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
